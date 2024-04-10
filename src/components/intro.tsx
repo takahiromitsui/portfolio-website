936660e64d1cefb52cd657966f73e0bb03e109d1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-10 15:12:14 +0200

    feat: add intro sentences

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 export default function Intro() {
 	return (
-		<section>
+		<section className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
 			<div className='flex items-center justify-center'>
 				<div className='relative'>
 					<motion.div
@@ -53,6 +53,18 @@ export default function Intro() {
 					</motion.div>
 				</div>
 			</div>
+			<motion.h1
+				className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+			>
+				<span className='font-bold'>Hello, I'm Takahiro.</span> I'm a{' '}
+				<span className='font-bold'>full-stack developer</span> with{' '}
+				<span className='font-bold'>4 years</span> of experience. I thrive on
+				creating <span className='italic'>web applications</span>. My expertise
+				lies in <span className='underline'>Python (FastAPI/Flask)</span> and{' '}
+				<span className='underline'>Typescript/JavaScript (NextJS)</span>.
+			</motion.h1>
 		</section>
 	);
 }
