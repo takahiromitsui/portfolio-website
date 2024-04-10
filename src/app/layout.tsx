e769d642dbe7fa8405555b79c37022d63d9650c6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-10 14:11:22 +0200

    feat: add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -29,7 +30,7 @@ export default function RootLayout({
 				sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
 				'
 				></div>
-
+				<Header />
 				{children}
 			</body>
 		</html>
