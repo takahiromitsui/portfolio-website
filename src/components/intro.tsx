b7be409907639dc5919b2e3fd6f6d949861a6d48
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-10 15:37:41 +0200

    feat: add buttons on intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -2,6 +2,10 @@
 import Image from 'next/image';
 import portrait from '../../public/portrait.jpg';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
 	return (
@@ -63,8 +67,47 @@ export default function Intro() {
 				<span className='font-bold'>4 years</span> of experience. I thrive on
 				creating <span className='italic'>web applications</span>. My expertise
 				lies in <span className='underline'>Python (FastAPI/Flask)</span> and{' '}
-				<span className='underline'>Typescript/JavaScript (NextJS)</span>.
+				<span className='underline'>Typescript/JavaScript (Next.js)</span>.
 			</motion.h1>
+			<motion.div
+				className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{
+					delay: 0.1,
+				}}
+			>
+				<Link
+					className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+					href='#contact'
+				>
+					Contact me here <BsArrowRight />
+				</Link>
+				<a
+					className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
+					href='/CV.pdf'
+					download
+				>
+					Download CV{' '}
+					<HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+				</a>
+
+				<a
+					className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+					href='https://www.linkedin.com/in/takahiro-mitsui-b68238207'
+					target='_blank'
+				>
+					<BsLinkedin />
+				</a>
+
+				<a
+					className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+					href='https://github.com/takahiromitsui'
+					target='_blank'
+				>
+					<FaGithubSquare />
+				</a>
+			</motion.div>
 		</section>
 	);
 }
