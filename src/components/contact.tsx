56f9df113eaca8168507ad18a114c6c4d1fa3ad6
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-04-12 14:03:49 +0200

    feat delete form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -34,40 +34,8 @@ export default function Contact() {
 				Please contact me directly at{' '}
 				<a className='underline' href='mailto:takahiro.mitsui.career@gmail.com'>
 					takahiro.mitsui.career@gmail.com
-				</a>{' '}
-				or through this form.
+				</a>
 			</p>
-
-			<form
-				className='mt-10 flex flex-col dark:text-black'
-				action={async formData => {
-					const { data, error } = await sendEmail(formData);
-
-					if (error) {
-						toast.error(error);
-						return;
-					}
-
-					toast.success('Email sent successfully!');
-				}}
-			>
-				<input
-					className='h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
-					name='senderEmail'
-					type='email'
-					required
-					maxLength={500}
-					placeholder='Your email'
-				/>
-				<textarea
-					className='h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none'
-					name='message'
-					placeholder='Your message'
-					required
-					maxLength={5000}
-				/>
-				<SubmitBtn />
-			</form>
 		</motion.section>
 	);
 }
