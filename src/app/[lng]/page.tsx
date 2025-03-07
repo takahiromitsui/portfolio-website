14ff5572cf2d53dd59c1198749fc926d2753a9ef
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2025-03-07 16:01:54 +0100

    chore: update next

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,13 +1,21 @@
-'use client';
-import Link from 'next/link';
-import { useTranslation } from '../i18n/client';
+import About from '@/components/about';
+import Contact from '@/components/contact';
+import Experience from '@/components/experience';
+import Intro from '@/components/intro';
+import Projects from '@/components/projects';
+import SectionDivider from '@/components/section-divider';
+import Skills from '@/components/skills';
 
-export default function Page({ params: { lng } }: { params: { lng: string } }) {
-	const { t } = useTranslation(lng, 'page');
+export default function Home() {
 	return (
-		<>
-			<h1>{t('title')}</h1>
-			<Link href={`/${lng}/second-page`}>second page</Link>
-		</>
+		<main className='flex flex-col items-center px-4'>
+			<Intro />
+			<SectionDivider />
+			<About />
+			<Projects />
+			<Skills />
+			<Experience />
+			{/* <Contact /> */}
+		</main>
 	);
 }
