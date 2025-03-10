c284c90d1796585f48822425bde7e673c90dd053
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2025-03-10 09:37:36 +0100

    feat: update cv section

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -2,8 +2,7 @@
 import Image from 'next/image';
 import portrait from '../../public/portrait.jpg';
 import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithubSquare } from 'react-icons/fa';
 import { useSectionInView } from '@/lib/hooks';
@@ -16,8 +15,15 @@ export default function Intro({
 	params: { lng: string };
 }) {
 	const { ref } = useSectionInView('home');
-	const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 	const { t } = useTranslation(lng, 'intro');
+
+	const pdfs =
+		lng === 'en'
+			? [{ path: '/Takahiro.Mitsui.CV.pdf', label: 'CV' }]
+			: [
+					{ path: '/履歴書_三井貴洋.pdf', label: '履歴書' },
+					{ path: '/職務経歴書_三井貴洋.pdf', label: '職務経歴書' },
+			  ];
 	return (
 		<section
 			ref={ref}
@@ -85,7 +91,7 @@ export default function Intro({
 				<span className='underline'>
 					Python (FastAPI/Flask)
 				</span>
-				and{' '}
+				{t('and')}
 				<span className='underline'>
 					Typescript/JavaScript (Next.js/React/Node.js/NestJS)
 				</span>
@@ -99,24 +105,19 @@ export default function Intro({
 					delay: 0.1,
 				}}
 			>
-				{/* <Link
-					className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
-					href='#contact'
-					onClick={() => {
-						setActiveSection('Contact');
-						setTimeOfLastClick(Date.now());
-					}}
-				>
-					Contact me here <BsArrowRight />
-				</Link> */}
-				<a
-					className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
-					href='/CV.pdf'
-					download
-				>
-					Download CV{' '}
-					<HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
-				</a>
+				{pdfs.map(pdf => (
+					<a
+						key={pdf.path}
+						className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
+						href={pdf.path}
+						download
+					>
+						{lng === 'en'
+							? `${t('download')} ${pdf.label}`
+							: `${pdf.label} ${t('download')}`}
+						<HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+					</a>
+				))}
 
 				<a
 					className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
